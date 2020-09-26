6791cdb85b70679084518bd7353182f7ac55421d
paulosantama <paulovitor_pe@hotmail.com>
2020-09-26 16:32:17 -0300

    refactor: final em param

--- src/Cliente.java
+++ src/Cliente.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Cliente {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
         while (true) {
